945dfb689d3c1fa2534a7ab0d266e5c5a3868964
paarmin <parniaa@gmail.com>
2021-02-10 12:48:51 -0500

    created css files

--- routes/users.js
+++ routes/users.js
@@ -44,6 +44,30 @@ module.exports = (db) => {
     req.session.user_id = req.params.id;
     res.redirect("/");
   })
+  router.get("/signup", (req, res) => {
+    // const templateVars = {
+    //   userObject: req.session["id"]
+    // };
+    res.render("signup");
+  });
+  // router.post("/", (req, res) => {
+  //   // If the e-mail or password are empty strings,
+  //   const email = req.body.email;
+  //   const password = req.body.password;
+  //   // console.log(email, password)
+  //     const userInfo = {
+  //       id: `123`,
+  //       email: email,
+  //       password: password
+  //     };
+
+  //     //Adding the user to the database
+  //     // console.log(userInfo);
+  //     req.session.id = userInfo.id;
+  //     res.redirect('/api/maps');
+
+  // });
+
   router.post("/logout", (req, res) => {
     req.session = null;
     res.redirect("/");
