4c09cafed86909858618dd4e8b18f96e13754a9d
Stephan Truchsess <stephantruchsess@gmail.com>
2021-02-11 19:33:33 -0500

    Linting file

--- routes/users.js
+++ routes/users.js
@@ -14,13 +14,12 @@ module.exports = (db) => {
     if(req.session.user_id) {
       return res.redirect('/');
     }
-    const templateVars = {user: req.session.user_id}
-    res.render('login', templateVars)
-  })
+    const templateVars = {user: req.session.user_id};
+    res.render('login', templateVars);
+  });
 
   router.post("/login", (req, res) => {
     const username = req.body.email;
-    console.log(username)
     db.query(`SELECT users.id FROM users WHERE users.email = $1;`, [username])
     .then((data) => {
       if(data.rows[0]){
@@ -30,19 +29,19 @@ module.exports = (db) => {
         return res.send('This user does not exist');
       }
     })
-  })
+  });
 
   router.get("/login/:id", (req, res) => {
     req.session.user_id = req.params.id;
     res.redirect("/");
-  })
+  });
 
   router.get("/signup", (req, res) => {
     if(req.session.user_id) {
       return res.redirect('/');
     }
-    const templateVars = {user: req.session.user_id}
-    res.render('signup', templateVars)
+    const templateVars = {user: req.session.user_id};
+    res.render('signup', templateVars);
   });
 
   router.post("/signup", (req, res) => {
@@ -68,97 +67,99 @@ module.exports = (db) => {
   router.post("/favourites/add", (req, res) => {
     const user_id = req.session.user_id;
     const map_id = req.body["map-id"];
-    const values = [user_id, map_id]
+    const values = [user_id, map_id];
     db.query(`INSERT INTO favourites (user_id, map_id) VALUES ($1, $2)`, values)
     .then(res => res.rows)
-    res.redirect(`/api/users/${user_id}/favourites`)
-  })
+    res.redirect(`/api/users/${user_id}/favourites`);
+  });
 
   router.get("/data/:id", (req, res) => {
     const id = req.params.id;
     db.query(`SELECT * FROM favourites JOIN users ON user_id = users.id WHERE user_id = $1`, [id])
     .then((data) => {
-      return res.json(data.rows)
+      return res.json(data.rows);
     })
-  })
+  });
 
   router.post("/favourites/delete", (req,res) => {
     const mapToRemove = Object.keys(req.body)[0];
     const user_id = req.session.user_id;
+    console.log("map toremove", mapToRemove)
+    console.log("userid", user_id)
     db.query(`DELETE FROM favourites WHERE map_id = $1 AND favourites.user_id= $2`, [mapToRemove, user_id])
     .then(() => {
       res.redirect(`/api/users/${user_id}/favourites`)
     })
-  })
+  });
 
   router.get("/my/favourites", (req, res) => {
     if(req.session.user_id){
       const id = req.session.user_id;
       return res.redirect(`/api/users/${id}/favourites`);
     }
-    const templateVars = {user: req.session.user_id}
+    const templateVars = {user: req.session.user_id};
     res.render('login', templateVars);
-  })
+  });
 
   router.get("/:id/favourites", (req, res) => {
     const id = req.params.id;
     const userId = req.session.user_id;
     db.query(`SELECT favourites.*, maps.* FROM favourites JOIN maps ON maps.id=favourites.map_id WHERE favourites.user_id = $1;`, [id])
     .then((data) => {
-      let templateVars = {user: req.session.user_id}
-      let parsedData = {}
+      let templateVars = {user: req.session.user_id};
+      let parsedData = {};
       data.rows.forEach(row => {
-        rowObject = {}
-        rowObject.name = row.name
-        rowObject.image = row.image_url
-        rowObject.id = row.id
-        rowObject.currentUserId = userId
+        rowObject = {};
+        rowObject.name = row.name;
+        rowObject.image = row.image_url;
+        rowObject.id = row.id;
+        rowObject.currentUserId = userId;
         rowObject.userId = id;
-        parsedData[row.id] = rowObject
+        parsedData[row.id] = rowObject;
       })
-      templateVars["data"] = parsedData
+      templateVars["data"] = parsedData;
       if (userId) {
-        return res.render("user_favourites", templateVars)
+        return res.render("user_favourites", templateVars);
       }
-      res.redirect("/api/users/login")
+      res.redirect("/api/users/login");
     })
     .catch(e => console.log(e))
   });
 
   router.get("/my/maps", (req, res) => {
-    const templateVars = {user: req.session.user_id}
+    const templateVars = {user: req.session.user_id};
     if(req.session.user_id){
       const id = req.session.user_id;
       return res.redirect(`/api/users/${id}/maps`);
     }
     res.render('login', templateVars);
-  })
+  });
 
   router.get("/:id/maps", (req, res) => {
-    const id = req.params.id
+    const id = req.params.id;
     db.query(`SELECT maps.* FROM maps JOIN pins ON maps.id = map_id WHERE pins.user_id = $1;`, [id])
     .then((data) => {
-      let templateVars = {user: req.session.user_id}
-      let parsedData = {}
-      console.log(templateVars)
+      let templateVars = {user: req.session.user_id};
+      let parsedData = {};
       data.rows.forEach(row => {
-        rowObject = {}
-        rowObject.name = row.name
-        rowObject.image = row.image_url
-        rowObject.id = row.id
-        parsedData[row.id] = rowObject
+        rowObject = {};
+        rowObject.name = row.name;
+        rowObject.image = row.image_url;
+        rowObject.id = row.id;
+        parsedData[row.id] = rowObject;
       })
       templateVars["data"] = parsedData;
-      return res.render("user_maps", templateVars)
+      return res.render("user_maps", templateVars);
     })
   });
+
   router.get("/my/profile", (req, res) => {
     if(req.session.user_id){
       const id = req.session.user_id;
       return res.redirect(`/api/users/${id}/profile`);
     }
     res.render('login');
-  })
+  });
 
   router.get("/:id/profile", (req, res) => {
     const id = req.params.id;
@@ -166,7 +167,6 @@ module.exports = (db) => {
     db.query(`SELECT users.email, users.id as userID, favourites.*, maps.* FROM favourites JOIN maps ON maps.id=favourites.map_id JOIN users ON users.id = favourites.user_id WHERE favourites.user_id = $1;`, [id])
     .then((data) => {
       let templateVars = {user: req.session.user_id};
-      console.log(data.rows)
       templateVars.userName = data.rows[0].email;
       templateVars.userMaps = data.rows[0].userid;
       let parsedData = {};
@@ -180,14 +180,12 @@ module.exports = (db) => {
         parsedData[row.id] = rowObject;
       })
       templateVars["data"] = parsedData;
-      // console.log(templateVars)
       if (userId) {
         return res.render("user_profile", templateVars);
       }
       res.redirect("/api/users/login");
     })
     .catch(e => console.log(e));
-
   });
   return router;
 };
